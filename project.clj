e433b456052c6cda7478d453c7b42b9d1d8bbfe3
Phillip Lord <phillip.lord@russet.org.uk>
2017-07-02 21:28:48 +0100

    Prepare for next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject primitive-operator "0.1"
+(defproject primitive-operator "0.2-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
