b1115bc70f08a073e4877b49e70bb21efe80a423
Phillip Lord <phillip.lord@russet.org.uk>
2017-07-10 14:22:58 +0100

    Add Javac target to support JDK before 1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject primitive-operator "0.2-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :source-paths ["src/clj"]
-  :java-source-paths ["src/java"])
+  :java-source-paths ["src/java"]
+  :javac-options     ["-target" "1.6" "-source" "1.6"])
